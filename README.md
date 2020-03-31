915dec55805fff3c66d85640241ed3cccd550f3e
Suirad <suirad@users.noreply.github.com>
2020-03-31 14:41:41 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# bookpress
-Adds a printing press node to minetest
+====Printing Press===
+Version 1.00
+
+Adds a printing press node which copies books at the cost of three paper
+
+Crafting:
+W = wood
+C = copper ingot
+I = iron ingot
+
+press:
+
+W,W,W
+W,I,W
+W,C,W
+
+Changelog:
+
+1.0: initial version with printing press
+
+Contact me at discord @The32bitguy#7553 for help, suggestions and bug reports
+
+================================================================
+License of source code and textures: LGPL v3
+
+Copyright © 2007 Free Software Foundation, Inc. <https://fsf.org/>
+
+Everyone is permitted to copy and distribute verbatim copies of this license document, but changing it is not allowed.
+
+This version of the GNU Lesser General Public License incorporates the terms and conditions of version 3 of the GNU General Public License, supplemented by the additional permissions listed below.
+=================================================================
+
+Textures:
+bookpress textures by @RektTangle/CaptainKlutz#3611 
+Sounds:
+print.1.ogg CC0 https://freesound.org/people/mark646/sounds/209217/
---
+++ init.lua
@@ -0,0 +1,53 @@
+-- This replicates books if you have a written book and paper
+minetest.register_node(
+    "bookpress:copy",
+    {
+        description = "Printing press",
+        tiles = {
+            "bookpress_bottom.png", -- y+
+            "bookpress_bottom.png", -- y-
+            "bookpress_right.png", -- x+
+            "bookpress_left.png", -- x-
+            "bookpress_back.png", -- z+
+            "bookpress_front.png" -- z-
+        },
+        is_ground_content = true,
+        groups = {cracky = 1},
+        on_rightclick = function(pos, node, player, itemstack, pointed_thing)
+            if (itemstack:get_name() == "default:book_written") then -- Don't want to copy anything other than books
+                local inv = player:get_inventory()
+
+                if (inv:contains_item("main", "default:paper 3")) then --You need paper -- Give book copy, take paper and send message
+                    inv:add_item("main", itemstack:peek_item(1))
+                    inv:remove_item("main", "default:paper 3")
+                    minetest.chat_send_player(player:get_player_name(), "The book is copied ")
+                     --Plays a sound at the nodes position
+                     printpos = pos
+                        minetest.sound_play("press", {
+                            pos = {x=printpos.x, y=printpos.y, z=printpos.z},
+	                        max_hear_distance = 20,
+	                        gain = 8.0,
+                         })
+                else
+                    minetest.chat_send_player(
+                        player:get_player_name(),
+                        "You need three sheets of paper to copy the book"
+                    )
+                end --End if statement to check for paper
+            else -- You need a default:book_written to copy
+                minetest.chat_send_player(player:get_player_name(), "You need a written book to copy")
+            end --end of if statement checking for book
+        end -- end of on_rightclick
+    }
+)
+
+-- Recipe for the cigarette
+minetest.register_craft({
+    type = "shaped", 
+    output = "bookpress:copy",
+    recipe = {
+        {"group:wood", "group:wood", "group:wood",},
+        {"group:wood", "default:copper_ingot", "group:wood",},
+        {"group:wood", "default:copper_ingot", "group:wood",},
+    }
+})
