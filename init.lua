9a0ba1eb9ad888318a3372801e0ddd55b7681bbc
The32bitguy <35638613+The32bitguy@users.noreply.github.com>
2020-04-11 23:15:49 +0100

    Made bookpress not eat paper magically

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 minetest.register_node(
     "bookpress:copy",
     {
-        description = "Printing Press",
+        description = "Printing press",
         tiles = {
             "bookpress_bottom.png", -- y+
             "bookpress_bottom.png", -- y-
@@ -14,35 +14,30 @@ minetest.register_node(
         is_ground_content = true,
         groups = {choppy = 2},
         on_rightclick = function(pos, node, player, itemstack, pointed_thing)
-            local pname = player:get_player_name()
-            -- Don't want to copy anything other than books
-            if not itemstack:get_name() == "default:book_written" then
-               minetest.chat_send_player(
-                  pname, "Only written books can be copied."
-               )
-               return itemstack
-            end
+            if (itemstack:get_name() == "default:book_written") then -- Don't want to copy anything other than books
+                local inv = player:get_inventory()
 
-            local inv = player:get_inventory()
-            -- You need paper, give book copy, take paper and send message
-            if inv:contains_item("main", "default:paper 3") then
-               player_api.give_item(player, itemstack:peek_item(1))
-               inv:remove_item("main", "default:paper 3")
-               minetest.chat_send_player(pname, "The book has been copied.")
-               -- Play a sound at the nodes position
-               local printpos = pos
-               minetest.sound_play("press", {
-                                      pos = {x=printpos.x, y=printpos.y, z=printpos.z},
-                                      max_hear_distance = 20,
-                                      gain = 8.0,
-               })
-            else
-               minetest.chat_send_player(
-                  pname,
-                  "You need three sheets of paper in your hotbar to copy a book."
-               )
-            end
-        end
+                if (inv:contains_item("main", "default:paper 3")) then --You need paper -- Give book copy, take paper and send message
+                    inv:add_item("main", itemstack:peek_item(1))
+                    inv:remove_item("main", "default:paper 3")
+                    minetest.chat_send_player(player:get_player_name(), "The book is copied ")
+                     --Plays a sound at the nodes position
+                     printpos = pos
+                        minetest.sound_play("press", {
+                            pos = {x=printpos.x, y=printpos.y, z=printpos.z},
+	                        max_hear_distance = 20,
+	                        gain = 8.0,
+                         })
+                else
+                    minetest.chat_send_player(
+                        player:get_player_name(),
+                        "You need three sheets of paper to copy the book"
+                    )
+                end --End if statement to check for paper
+            else -- You need a default:book_written to copy
+                minetest.chat_send_player(player:get_player_name(), "You need a written book to copy")
+            end --end of if statement checking for book
+        end -- end of on_rightclick
     }
 )
 
